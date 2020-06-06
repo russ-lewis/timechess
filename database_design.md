336831ad3309c0997429f1e356873b66766c078e
Russ Lewis <russl5445@gmail.com>
2020-06-06 04:12:04 +0000

    Wrote my wrapper for the chess.Board class, ready for experimental use in the program.

--- database_design.md
+++ database_design.md
@@ -6,9 +6,26 @@ TABLE: games
   INTEGER white FOREIGN KEY (players.id) NOT NULL
   INTEGER black FOREIGN KEY (players.id) NOT NULL
  
- TABLE: moves
+TABLE: moves
    # PRIMARY_KEY(gameID,num,color)
    INTEGER    gameID FOREIGN KEY (games.id)
    INTEGER(3) num    NOT NULL
    CHAR       color  NOT NULL
    VARCHAR(7) move   NOT NULL   # use standard encoding (except that you can *PREFIX* with ~ to indicate an invalidated move).  Worst case: cxd8=Q  Special cases: WhRes BlRes Draw.  No "notes", such as "+" (for Check), "e.p.", or commentary.
+
+
+
+TABLE: log
+    # probably a transient table, but useful for debugging; instead of
+    # storing the *current* state of each game, it instead stores the list of
+    # *changes* over time for each game, so that we can rebuild the history
+    # needed.  NOTE: It does *not* record invalidations, just user actions;
+    # since invalidations can be rebuilt as-needed from the history
+
+    INTEGER    id     PRIMARY KEY NOT NULL AUTO_INCREMENT
+    INTEGER    gameID FOREIGN KEY (games.id)
+    INTEGER(3) num    NOT NULL
+    CHAR       color  NOT NULL
+    VARCHAR(6) move   NOT NULL
+    
+
---
+++ server/timechess_game.py
@@ -0,0 +1,66 @@
+
+import chess   # standard library: python-chess
+
+
+
+NOTES:
+
+
+
+class TimeChess_Game:
+    """Models the history of a TimeChess game; it has a sequence of moves
+       (some of which are invalidated, or are "pass").  It keeps a chess.Board
+       object inside it, which represents the state of the board at the end of
+       the sequence; you can add new moves to the history (finding out, as you
+       go, which need to be invalidated).
+    """
+
+    def __init__(self):
+        self.board = chess.Board()
+
+    def add_to_history(self, move):
+        """Takes a move, and tries to apply it.  Give the old version of the
+           move (including the invalidation mark) if necessary.  Returns the
+           new version of the move (as a string) IF IT CHANGES.  Returns None
+           otherwise.
+        """
+
+        # the caller must check for this!
+        assert not self.board.is_game_over()
+
+        if move == "pass":
+            self.board.push(chess.Move.null())
+            return None
+
+        if move[0] == '~':
+            base_move = move[1:]
+        else:
+            base_move = move
+
+        # attempt the move.  If it's not valid in the current configuration, then
+        # a ValueError will be thrown; in that case, push a null move instead.  In
+        # either case, record the new_move as the result: either base_move, or an
+        # invalidated version of it.
+
+        try:
+            self.board.push_san(base_move)
+            # if we get here, the move succeeded!
+            new_move = base_move
+        except ValueError:
+            # if we get here, the move was illegal.
+            new_move = '~'+base_move
+
+        # should the caller update the database?  We tell them by either returning
+        # None (no change), or the updated move string.
+        if new_move == move:
+            return None
+        else:
+            return new_move
+
+    def is_game_over(self):
+        return self.board.is_game_over()
+
+    def legal_moves(self):
+        return self.board.legal_moves
+
+
