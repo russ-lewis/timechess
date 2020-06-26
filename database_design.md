6cc3312fc29e6e8b7bab9fe439bb6095bfa6ef74
Russ Lewis <russl5445@gmail.com>
2020-06-26 19:05:42 +0000

    Working on implementing the record-moves logic

--- database_design.md
+++ database_design.md
@@ -1,31 +1,89 @@
-The main game database will need to store a little bit of common game state, plus
-a move history for each game.
+# The sessions table maps cookies to google accounts.  Sessions can be
+# established without a Google account; however, when the user says "log in
+# with Google," we go through the OAuth process to figure out their google
+# account name.  Note that the google account does not necessarily link the
+# user to an account name here on the site; that is done through the Player
+# table, which comes next.  But, if the user *has* created a player already,
+# then as soon as the session is associated with a google account, the player
+# will already be logged in "as" the player.
 
+TABLE: sessions
+  INTEGER(64) sessionID  PRIMARY KEY NOT NULL     # randomly selected
+  DATETIME    expiration             NOT NULL
+  CHAR(128)   google_account
+
+TABLE: oauth_state
+  -- TBD --
+
+# Once a person has logged in through OAuth, the session knows their google
+# account name.  But until they actually "create" an account on our site,
+# that name has no significance.  After they create that account, then the
+# process of logging in through OAuth will be (from the user perspective) the
+# same thing as "logging into timechess"
+#
+# While the id# is the underlying ID of the player on the site (although
+# never shown to the user) - and thus the ID is the primary key of this table -
+# we need to remember that we will *continually* be mapping session IDs to
+# player IDs, and then to player names.  So maybe we need to add an index for
+# the google_account as well?  Or maybe, we want to have a Redis cache, and
+# maybe the Redis cache can contain the player ID and player name, not just
+# the google account.  (Or maybe, even *instead of* the google account!)
+# Solution TBD.
+
+TABLE: players
+  INTEGER     id             PRIMARY KEY NOT NULL AUTO_INCREMENT
+  CHAR(128)   google_account             NOT NULL
+  CHAR(64)    player_name                NOT NULL       # what people see publicly
+
+# this keeps track of ongoing games.  Later, I'm sure I'll add many fields,
+# but for now, it just maps a game ID to the two player IDs involved.
+# Note that it does *NOT* (yet) have any information about the end-state of
+# the game; if the game ends, we leave the record in this table, and simply
+# stop accepting new moves.
+ 
 TABLE: games
-  INTEGER id    PRIMARY KEY              NOT NULL AUTO_INCREMENT
-  INTEGER white FOREIGN KEY (players.id) NOT NULL
-  INTEGER black FOREIGN KEY (players.id) NOT NULL
+  INTEGER id    PRIMARY KEY             NOT NULL AUTO_INCREMENT
+  INTEGER white FOREIGN KEY players(id) NOT NULL
+  INTEGER black FOREIGN KEY players(id) NOT NULL
+
+# this keeps track of the moves which have happened, over time.  All of the
+# moves, from all games, are mixed into a single table.  Each move is
+# associated with a gameID, a move number, and a sequence number.  (These
+# three fields together are the primary key.)  The game ID is a reference to
+# the 'games' table; the move number counts half-moves, with the first
+# half-move of white being move 1.  (Thus, the very first move of black -
+# their first half-move - is move 2.)  The sequence number is used to record
+# changes over time; the very first time that a given half-move number is
+# used in a game, it is seq# 1; if it is ever changed, then the old record
+# persists, but a new record with seq# 2 is created.
+#
+# The actual moves are encoded with standard algebraic notation, generally;
+# we support the special strings WhRes BlRes Draw Pass.  Invalidated moves are
+# not recorded in the database; we simply discover that they are invalidated
+# when we apply the moves to a Board object in Python.  Check is *not*
+# recorded in the database, but checkmate *is*, with a ++ suffix.
+#
+# The worst-case move is thus:
+#      cxd8=Q++
+# which is 8 characters.  This is why we set the range of the field to 8.
+#
+# FINAL NOTE: The debug_seq field is an auto-incrementing counter, which
+#             simply exists so that we can re-create changes that happened to
+#             the database over time.  Our current design is that all of the
+#             records in this database are immutable once created, and if we
+#             don't violate that rule with some DELETEs or UPDATEs, we will
+#             be able to rebuild the history and recreate old bugs.
+#
+# UPDATE: I wanted to use AUTO_INCREMENT on debug_seq, but this is not allowed
+#         in SQL (the AUTO field must be part of the key).  So we're going to
+#         have to set the debug_seq by hand.  I don't know exactly how I want
+#         to do that, right now.
  
 TABLE: moves
-   # PRIMARY_KEY(gameID,num,color)
-   INTEGER    gameID FOREIGN KEY (games.id)
-   INTEGER(3) num    NOT NULL
-   CHAR       color  NOT NULL
-   VARCHAR(7) move   NOT NULL   # use standard encoding (except that you can *PREFIX* with ~ to indicate an invalidated move).  Worst case: cxd8=Q  Special cases: WhRes BlRes Draw.  No "notes", such as "+" (for Check), "e.p.", or commentary.
-
-
-
-TABLE: log
-    # probably a transient table, but useful for debugging; instead of
-    # storing the *current* state of each game, it instead stores the list of
-    # *changes* over time for each game, so that we can rebuild the history
-    # needed.  NOTE: It does *not* record invalidations, just user actions;
-    # since invalidations can be rebuilt as-needed from the history
-
-    INTEGER    id     PRIMARY KEY NOT NULL AUTO_INCREMENT
-    INTEGER    gameID FOREIGN KEY (games.id)
-    INTEGER(3) num    NOT NULL
-    CHAR       color  NOT NULL
-    VARCHAR(6) move   NOT NULL
-    
+   # PRIMARY_KEY(gameID,halfMoveNum,seqNum))
+   INTEGER    gameID         FOREIGN KEY REFERENCES games(id)
+   INTEGER(3) halfMoveNum    NOT NULL
+   INTEGER(1) seqNum         NOT NULL
+   VARCHAR(8) move           NOT NULL
+   INTEGER    debug_seq      NOT NULL AUTO_INCREMENT
 
--- server/timechess.py
+++ server/timechess.py
@@ -42,8 +42,8 @@ def index():
 
 
 
-@app.route("/game/<int:gameID>/<int:pos>")
-def game(gameID, pos):
+@app.route("/game/<int:gameID>/<int:halfMoveNum>")
+def game(gameID, halfMoveNum):
     db = get_db()
     (sessionID, google_account) = sessions.get_session(db,
                                                lambda key: request.cookies.get(key),
@@ -61,7 +61,7 @@ def game(gameID, pos):
     game.add_to_history("e4")
     game.add_to_history("e5")
 
-    legal_moves = game.legal_moves_san()
+    legal_moves = sorted(game.legal_moves_san())
 
     def piece_url(sym):
         color = 'l' if sym.isupper() else 'd'
@@ -72,7 +72,7 @@ def game(gameID, pos):
 
     return render_template("game.html",
                            gameID=gameID,
-                           history=history, pos=pos,
+                           history=history, halfMoveNum=halfMoveNum,
                            legal_moves=legal_moves,
                            board=str(game.board), pieces=pieces)
 
@@ -80,6 +80,137 @@ def game(gameID, pos):
 
 @app.route("/move", methods=["POST"])
 def move():
-    return "Hello world"
+    db = get_db()
+    (sessionID, google_account) = sessions.get_session(db,
+                                               lambda key: request.cookies.get(key),
+                                               None,
+                                               ["google_account"])
+
+    # did they pass the required variables?
+    if "gameID" not in request.form or "halfMoveNum" not in request.form or "move" not in request.form:
+        TODO
+    gameID      = int(request.form["gameID"])
+    halfMoveNum = int(request.form["hmNum" ])
+    newMove     =     request.form["move"  ]
+
+    if gameID <= 0 or halfMoveNum <= 0 or len(move) == 0:
+        TODO
+
+    # is the current logged-in person a player on the site?
+    cursor = db.cursor()
+    cursor.execute("SELECT players.id FROM sessions,players WHERE sessions.id=%s AND players.google_acount=sessions.google_account", (sessionID,))
+    rows = cursor.fetchall()
+    cursor.close()
+    if len(rows) != 1:
+        TODO
+    playerID = rows[0][0]
+
+    # get basic game info.  Only a player in the game can play, and they can
+    # only choose their own moves.
+    cursor = db.cursor()
+    cursor.execute("SELECT white,black FROM games WHERE id=%s", (gameID,))
+    rows = cursor.fetchall()
+    cursor.close()
+    if len(rows) != 1:
+        TODO
+    wh,bl = rows[0]
+    assert wh != bl
+
+    # is the player not in the game?
+    if playerID not in (wh,bl):
+        TODO
+
+    # is the player attempting to move for the other side?
+    if playerID == wh:
+        if halfMoveNum % 2 != 1:
+            TODO
+    else:
+        if halfMoveNum % 2 != 0:
+            TODO
+
+    # get the list of moves from the database.  This will report only a
+    # *single* move for each halfMoveNum (the most recent one), but it will
+    # include the seqNum for each, since that is useful later.  Note that this
+    # is only a DB search; it does *NOT* know which of these moves might be
+    # invalidated.  This does *NOT* look for checkmate anywhere - it returns
+    # the *entire* history, even if there is a checkmate in the middle.
+    #
+    # Also note that the first element is always None, so that the first
+    # halfMove - numbered 1 for user convenience - will be in slot [1] of the
+    # returned list.  Thus, an empty list is invalid, but a list with only one
+    # element is valid; that simply means that the game has no moves.  (If you
+    # ask it for an invalid game ID, this is what you will get - it does *NOT*
+    # validate that the gameID is valid.  But that's no worry in this code,
+    # since we validated the gameID above.)
+    #
+    # Note that each element (save the None in [0]) is a tuple: (move,seqNum)
+    moves = get_moves(gameID)
+
+    # is the move too advanced - no connection to it in the history?
+    if halfMoveNum > len(moves):
+        TODO
+
+    # is there already a checkmate, anywhere in the move list?  Or any
+    # end-of-game marker at all?
+    if move_list_has_game_end(moves):
+        TODO
+
+    # have we already had too many alterations to that move (our DB table can
+    # only hold seqNums up to 9).  Note that this is also useful, even if the
+    # user is not doing anything bad, since we'll need to *set* the seqNum in
+    # the new row we insert soon.
+    assert type(moves[halfMoveNum][1]) == int   # did we remember to conver it?
+    if moves[halfMoveNum][1] == 9:
+        TODO
+
+    # is the move in question a "Pass"?  If so, then the player has lost the
+    # chance to move, and can never change it.
+    if moves[halfMoveNum][0] == "Pass":
+        TODO
+
+
+    # looks like (hopefully) we're going to add an update to the database.  To
+    # ensure that this is a legal move (and to check for checkmate as a result
+    # of this new move), we need to build a Board object which represents the
+    # current state (as of the position where we're making a change).
+    board = build_board_from_moves(moves[:halfMoveNum])
+
+    # check to see if the move is legal, given the history that comes
+    # before it in the game.
+    if not board.move_is_legal(newMove)
+        TODO
+
+    # apply the move, and check for checkmate.
+    board.apply_move(newMove)
+
+    if board.is_checkmate:
+        newMove += "++"
+        TODO
+    if board.game_ended:
+        TODO
+
+
+    # all looks good.  Record the new move into the DB.  (Note the cool SQL
+    # nested query, to set the debug_seq field...this is a new thing for me to
+    # try out, and I sure hope I get it right!) - Russ, 26 Jun 2020
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO moves(gameID,halfMovNum,seqNum,move,debug_seq) "+
+                   "VALUES(%s,%s,%s,%s," +
+                          "1+SELECT MAX(debug_seq) FROM moves)",
+                   (gameID, halfMoveNum, 1+moves[halfMoveNum][1], newMove))
+    if cursor.rowcount != 1:
+        TODO
+    cursor.close()
+
+
+    # yay, all done!
+    db.commit()
+
+    # TODO: wake up Redis server, let it know that a new move has happened.
+
+
+    return redirect(url_for("game",
+                            gameID      = gameID,
+                            halfMoveNum = halfMoveNum+1))
 
 
